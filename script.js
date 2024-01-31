ebff52f80bb1da2a2a8303932911293e19ebe7bb
Helio Vallim <h.vallim@gmail.com>
2024-01-30 21:16:09 -0300

    feat: criação da função para checagem de palíndromos

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function isPalindrome(str) {
+  const re = /[\W_]/g;
+  const lowRegStr = str.toLowerCase().replace(re, "");
+  const reverseStr = lowRegStr.split("").reverse().join("");
+  return reverseStr === lowRegStr;
+}
 
+document.querySelector("#check-btn").addEventListener("click", () => {
+  const inputValue = document.querySelector("#text-input").value;
+  if (!inputValue) {
+    alert("Please input a value");
+  } else {
+    return (document.querySelector("#result").innerText = `${inputValue} ${
+      isPalindrome(inputValue) ? " is a palindrome" : " is not a palindrome"
+    }`);
+  }
+});
+
+// const result = isPalindrome(inputValue) ? ' is a palindrome' : ' is not a palindrome';
+//     document.querySelector('#result').innerText = `${inputValue} ${result}`;
